d2b8263540f4e1a26b217f2aadf4e7c37971eb0e
Konstantin <guzarov@gmail.com>
2021-03-07 00:38:26 +0700

    task3 - add prometheus instrumentation

---
+++ task3/app-spring-prometheus/src/main/java/otus/task2/data/UserService.java
@@ -0,0 +1,42 @@
+package otus.task2.data;
+
+import org.springframework.stereotype.Service;
+import otus.task2.data.model.UserEntity;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private final UserRepository repository;
+
+    public UserService(UserRepository repository) {
+        this.repository = repository;
+    }
+
+    public UserEntity createUser(String username, String firstName, String lastName, String email, String phone) {
+        return repository.save(new UserEntity(username, firstName, lastName, email, phone));
+    }
+
+    public boolean deleteUser(Long userId) {
+        return repository.findById(userId).map(user -> {
+                    repository.delete(user);
+                    return user;
+        }).isPresent();
+    }
+
+    public Optional<UserEntity> findUser(Long userId) {
+        return repository.findById(userId);
+    }
+
+    public Optional<UserEntity> updateUser(Long userId, String username, String firstName, String lastName, String email, String phone) {
+        return repository.findById(userId).map(entity -> {
+            entity.setUsername(username);
+            entity.setFirstName(firstName);
+            entity.setLastName(lastName);
+            entity.setEmail(email);
+            entity.setPhone(phone);
+            return Optional.of(repository.save(entity));
+        }).orElse(Optional.empty());
+    }
+}
